5d988be09d9d31d34488f55b25c73d2535275d17
Will Carter <will.carter@fergusllc.com>
2019-12-18 20:03:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array)
   # your code here
+  squares = []
   array.each do |item|
-    item = item * item
+    squares.push(item * item)
   end
+  return squares
 end
 
 puts (square_array([1,2,3]).inspect)
